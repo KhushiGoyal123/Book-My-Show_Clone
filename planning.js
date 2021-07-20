6dbbb04d80dc65c781f26871776dbb402db0e346
KhushiGoyal123 <khushi030201goyal@gmail.com>
2021-07-20 14:25:16 +0530

    home,movie,plays page added

---
+++ planning.js
@@ -0,0 +1,25 @@
+// Book my show planning
+
+// -> We need a reusable carousel
+//     -> Hero carousel
+//     -> Common carousel
+//     -> cast and crew small carousel
+
+
+// Pages
+//  -> Home Page
+//  -> Movie Page
+//  -> Categories Page
+//  -> Authentication Page
+//  -> Razorpay payment gateway
+
+
+// Plan -> How are we building it ???🤔
+// Layouts
+// Routes
+// Pages
+// Components
+
+
+// HOC - Higher Order Components -> will return a component instead of
+// jsx element 
